7351873f1b6c523bb4a665928f0cffc11d07fc09
Sam Bean <sam.h.bean@gmail.com>
2021-07-01 00:06:46 -0400

    goofy

---
+++ terraform/variables.tf
@@ -0,0 +1,48 @@
+variable "region" {
+  default="us-east-1"
+}
+
+variable "name_prefix" {
+  default=""
+}
+
+variable "postgres_db_name" {
+  default = "feast"
+}
+
+variable "postgres_db_user" {
+  default = "feast"
+}
+
+variable "map_accounts" {
+  description = "Additional AWS account numbers to add to the aws-auth configmap."
+  type        = list(string)
+
+  default = [
+  ]
+}
+
+variable "map_roles" {
+  description = "Additional IAM roles to add to the aws-auth configmap."
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+
+  ]
+}
+
+variable "use_persistent_emr_cluster" {
+  description = "Create a persistent EMR cluster."
+  default     = true
+}
+
+variable "tags" {
+  description = "Tags"
+  type        = map(string)
+
+  default = {}
+}
